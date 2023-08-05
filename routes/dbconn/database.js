be4ceba22d64948c54bfec92cb9d2ec151f8f97f
Aditya Dhayfule <adityadhayfule3101@gmail.com>
2023-08-05 13:24:45 +0530

    updated connections

--- routes/dbconn/database.js
+++ routes/dbconn/database.js
@@ -1,13 +1,13 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 let conn = null;
 
 module.exports = function () {
   if (!conn) {
-    conn = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "password",
-      database: "Hline",
+    conn = mysql.createPool({
+      host: "bawsid2ij5moefmc4vkg-mysql.services.clever-cloud.com",
+      user: "u59dm4pznjbqrdif",
+      password: "lzkJeNoeRlGKzyaquymH",
+      database: "bawsid2ij5moefmc4vkg",
     });
   }
   return conn;
